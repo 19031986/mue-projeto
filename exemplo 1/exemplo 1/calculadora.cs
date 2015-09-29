83b775c12375d9dad266ad6a3f2bdf399f6e628c
Ailton silva araujo <aiton_siva@outlook.com>
2015-09-29 15:50:57 -0300

    meu primeiro projeto

---
+++ exemplo 1/exemplo 1/calculadora.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo_1
+{
+    class calculadora
+    {
+        static void Main(string[] args)
+        {
+        }
+        public int soma (int a, int b)
+        { 
+          return a +b;
+            { 
+       static voice header {
+         padding: 15px 0px 30px 52px;
+            class color   footer {
+            padding: 25px 10px;
+                }
+    
+   
